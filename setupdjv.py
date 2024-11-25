5d453ef9c2924fc96416267267c9a81bf3d77a92
siva <snair.za@gmail.com>
2024-11-25 09:09:04 +0200

    use correct creds

---
+++ setupdjv.py
@@ -0,0 +1,10 @@
+from dejavu import Dejavu
+config = {
+    "database": {
+        "host": "127.0.0.1",
+        "user": "siva",
+        "password": "fastest", 
+        "database": "dejavu",
+    }
+}
+djv = Dejavu(config)
